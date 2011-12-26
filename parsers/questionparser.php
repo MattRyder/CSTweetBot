104ec547e25f9c62c48775dd09c8102f4e68ffaf
Matt Ryder <matt@mattryder.co.uk>
2011-12-26 07:58:32 +0000

    Added basic Mediawiki parsing

--- parsers/questionparser.php
+++ parsers/questionparser.php
@@ -6,6 +6,7 @@
  */
  
 require_once 'mathparser.php';
+require_once 'wikiparser.php';
  
 class QuestionParser
 {
@@ -18,6 +19,23 @@ class QuestionParser
 		$question = preg_replace("/[?|!]$/", '', $question);
 		
 		$qElements = preg_split("/[\s]/", $question, -1, PREG_SPLIT_NO_EMPTY);
+		
+		if(preg_match("/^w([ho]|[hom])/", $qElements[0]))
+		{
+			if(preg_match("/^([is]|[are]|[was]|[were])/", $qElements[1]))
+			{
+				//sentence so far is: "Who is/are"
+				//So we're going to try for a reference article!
+				for($i=2; $i < count($qElements); ++$i) 
+				{ 
+					if(strlen($referenceName) == 0) { $referenceName = $qElements[$i]; }
+					else $referenceName = $referenceName . '_' . $qElements[$i];
+				}
+				
+				return getWikiArticle($referenceName);
+			}
+		}
+				
 		
 		//Regex the first word for "who/what/when/where/why"
 		if(preg_match("/^wh([at]|[en]|[ere]|[hy])/", $qElements[0]))
@@ -32,12 +50,11 @@ class QuestionParser
 			}
 		}
 		
+		//They're trying to talk to a robot. *sigh*
 		if(preg_match("/^h([ello]|[ey]|[i])/", $qElements[0]))
 		{
 			//Return the token that tells CSTweetBot to send a hello message.
 			return "RETURN_GREETING_WITH_NAME";
 		}
 	}
-	
-	
 };
